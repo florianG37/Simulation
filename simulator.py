39d215416227692eb1d37a5afc4088676c20cead
pigmad <34888994+pigmad@users.noreply.github.com>
2020-10-30 15:33:55 +0100

    Changement gen loi expo

--- simulator.py
+++ simulator.py
@@ -1,5 +1,4 @@
-from math import exp
-from random import uniform, random
+from random import uniform, random, expovariate
 
 dateSimulation = 0
 numberBuses = 0
@@ -17,7 +16,7 @@ billBook = []
 class arrivalBus:
     def execute():
         global numberBuses
-        billBook.append((arrivalBus, dateSimulation + exp(1 / 2)))
+        billBook.append((arrivalBus, dateSimulation + expovariate(1/2)))
         numberBuses += 1
         billBook.append((arrivalQueueC, dateSimulation))
 
@@ -85,7 +84,7 @@ class start:
         numberBusesQueueR = 0
         statusControlCentre = False
         positionsRepairCentre = 0
-        billBook.append((arrivalBus, dateSimulation + exp(1 / 2)))
+        billBook.append((arrivalBus, dateSimulation + expovariate(1/2)))
         billBook.append((end, 160))
 
 
@@ -107,8 +106,9 @@ def updating_areas(date1, date2):
 dateSimulation = 0
 billBook.append((start, dateSimulation))
 while billBook:
-    eventDate = billBook[0]
+    eventDate = billBook[0]	
     del billBook[0]
     updating_areas(dateSimulation, eventDate[1])
     dateSimulation = eventDate[1]
+    print(eventDate[0])
     eventDate[0].execute()
