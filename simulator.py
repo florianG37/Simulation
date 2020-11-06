e424e62da0d5444a5f370d14c8448e5fecaf61c0
pigmad <34888994+pigmad@users.noreply.github.com>
2020-11-06 15:18:54 +0100

    Ajout export fichier texte

--- simulator.py
+++ simulator.py
@@ -1,6 +1,8 @@
 from random import uniform, random, expovariate
+from pathlib import Path
 
 dateSimulation = 0
+timeSimulation = 160
 numberBuses = 0
 numberBusesRepaired = 0
 areaNumberBusesQueueC = 0
@@ -10,8 +12,14 @@ numberBusesQueueC = 0
 numberBusesQueueR = 0
 statusControlCentre = False
 positionsRepairCentre = 0
+averageWaitingTimeControl = 0
+averageWaitingTimeRepair = 0
+rateUseRepair = 0
 billBook = []
 
+#Variables pour l'export
+fileName = "SimulatorRepairCenter.txt"
+billBookExport = []
 
 class arrivalBus:
     def execute():
@@ -19,7 +27,9 @@ class arrivalBus:
         billBook.append((arrivalBus, dateSimulation + expovariate(1/2)))
         numberBuses += 1
         billBook.append((arrivalQueueC, dateSimulation))
-
+		
+    def __str__(self):
+        return "Arrivée Bus"
 
 class arrivalQueueC:
     def execute():
@@ -27,7 +37,9 @@ class arrivalQueueC:
         numberBusesQueueC += 1
         if not statusControlCentre:
             billBook.append((accessControl, dateSimulation))
-
+			
+    def __str__(self):
+        return "Arrivée File Contrôle"
 
 class accessControl:
     def execute():
@@ -35,7 +47,9 @@ class accessControl:
         numberBusesQueueC -= 1
         statusControlCentre = True
         billBook.append((departureControl, dateSimulation + uniform(1 / 4, 13 / 12)))
-
+		
+    def __str__(self):
+        return "Accès Contrôle"
 
 class departureControl:
     def execute():
@@ -45,7 +59,9 @@ class departureControl:
             billBook.append((accessControl, dateSimulation))
         if random() < 0.3:
             billBook.append((arrivalQueueR, dateSimulation))
-
+			
+    def __str__(self):
+        return "Départ Contrôle"
 
 class arrivalQueueR:
     def execute():
@@ -55,6 +71,8 @@ class arrivalQueueR:
         if positionsRepairCentre < 2:
             billBook.append((accessRepair, dateSimulation))
 
+    def __str__(self):
+        return "Arrivée File Réparation"
 
 class accessRepair:
     def execute():
@@ -62,7 +80,9 @@ class accessRepair:
         numberBusesQueueR -= 1
         positionsRepairCentre += 1
         billBook.append((departureRepair, dateSimulation + uniform(2.1, 4.5)))
-
+	
+    def __str__(self):
+        return "Accès Réparation"
 
 class departureRepair:
     def execute():
@@ -70,7 +90,9 @@ class departureRepair:
         positionsRepairCentre -= 1
         if numberBusesQueueR > 0:
             billBook.append((accessRepair, dateSimulation))
-
+			
+    def __str__(self):
+        return "Depart Réparation"
 
 class start:
     def execute():
@@ -85,16 +107,35 @@ class start:
         statusControlCentre = False
         positionsRepairCentre = 0
         billBook.append((arrivalBus, dateSimulation + expovariate(1/2)))
-        billBook.append((end, 160))
-
+        billBook.append((end, timeSimulation))
+		
+    def __str__(self):
+        return "Debut"
 
 class end:
     def execute():
+        global averageWaitingTimeControl, averageWaitingTimeRepair, rateUseRepair
         billBook.clear()
         averageWaitingTimeControl = areaNumberBusesQueueC / numberBuses
         averageWaitingTimeRepair = areaNumberBusesQueueR / numberBusesRepaired
-        rateUseRepair = areaPositionsRepairCentre / (2 * 160)
+        rateUseRepair = areaPositionsRepairCentre / (2 * timeSimulation)
+	
+    def __str__(self):
+        return 'Fin'
 
+def export_result_to_txt():
+	global fileName, timeSimulation, numberBuses, numberBusesRepaired, numberBusesQueueC, numberBusesQueueR, averageWaitingTimeControl, averageWaitingTimeRepair, rateUseRepair, billBookExport
+	with open(fileName,"a+") as fic :
+		fic.write('Durée de la simulation : {}\n'.format(timeSimulation))
+		fic.write('Nombre de bus entrées : {}\n'.format(numberBuses))
+		fic.write('Nombre de bus réparés : {}\n'.format(numberBusesRepaired))
+		fic.write('Nombre de bus file controle : {}\n'.format(numberBusesQueueC))
+		fic.write('Nombre de bus file réparation : {}\n'.format(numberBusesQueueR))
+		fic.write('Temps moyen attente file controle : {}\n'.format(averageWaitingTimeControl))
+		fic.write('Temps moyen attente file réparation : {}\n'.format(averageWaitingTimeRepair))
+		fic.write('Taux d\'utilisation du centre de réparation : {}\n'.format(rateUseRepair))
+		fic.write('Echéancier de la simulation : {}\n\n'.format(billBookExport))
+ 
 
 def updating_areas(date1, date2):
     global areaNumberBusesQueueC, areaNumberBusesQueueR, areaPositionsRepairCentre
@@ -107,7 +148,9 @@ billBook.append((start, dateSimulation))
 while billBook:
     billBook.sort(key = lambda date: date[1]) 
     eventDate = billBook[0]	
+    billBookExport.append(billBook[0])
     del billBook[0]
     updating_areas(dateSimulation, eventDate[1])
     dateSimulation = eventDate[1]
     eventDate[0].execute()
+export_result_to_txt()
