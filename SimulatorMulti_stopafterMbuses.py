191b40cde6e85a57930c8e539675f8a1f182d9fc
dylan275 <dylan.motard@etu.univ-tours.fr>
2020-11-30 10:06:02 +0100

    Ajout de la saisie du nombre de bus par l'utilisateur

--- SimulatorMulti_stopafterMbuses.py
+++ SimulatorMulti_stopafterMbuses.py
@@ -1,17 +1,18 @@
 from random import uniform, random, expovariate
-from pathlib import Path
+
 
 class arrivalBus:
     def execute():
-        global numberBuses, stopBusNumer
-        billBook.append((arrivalBus, dateSimulation + expovariate(1/2)))
+        global numberBuses, stopBusNumber
+        billBook.append((arrivalBus, dateSimulation + expovariate(1 / 2)))
         numberBuses += 1
         billBook.append((arrivalQueueC, dateSimulation))
-        if (numberBuses == stopBusNumer):
+        if (numberBuses == stopBusNumber):
             billBook.append((end, dateSimulation))
-		
+
     def __str__(self):
         return "Arrivée Bus"
+
 
 class arrivalQueueC:
     def execute():
@@ -19,9 +20,10 @@ class arrivalQueueC:
         numberBusesQueueC += 1
         if not statusControlCentre:
             billBook.append((accessControl, dateSimulation))
-			
+
     def __str__(self):
         return "Arrivée File Contrôle"
+
 
 class accessControl:
     def execute():
@@ -29,9 +31,10 @@ class accessControl:
         numberBusesQueueC -= 1
         statusControlCentre = True
         billBook.append((departureControl, dateSimulation + uniform(1 / 4, 13 / 12)))
-		
+
     def __str__(self):
         return "Accès Contrôle"
+
 
 class departureControl:
     def execute():
@@ -41,9 +44,10 @@ class departureControl:
             billBook.append((accessControl, dateSimulation))
         if random() < 0.3:
             billBook.append((arrivalQueueR, dateSimulation))
-			
+
     def __str__(self):
         return "Départ Contrôle"
+
 
 class arrivalQueueR:
     def execute():
@@ -56,15 +60,17 @@ class arrivalQueueR:
     def __str__(self):
         return "Arrivée File Réparation"
 
+
 class accessRepair:
     def execute():
         global numberBusesQueueR, positionsRepairCentre
         numberBusesQueueR -= 1
         positionsRepairCentre += 1
         billBook.append((departureRepair, dateSimulation + uniform(2.1, 4.5)))
-	
+
     def __str__(self):
         return "Accès Réparation"
+
 
 class departureRepair:
     def execute():
@@ -72,9 +78,10 @@ class departureRepair:
         positionsRepairCentre -= 1
         if numberBusesQueueR > 0:
             billBook.append((accessRepair, dateSimulation))
-			
+
     def __str__(self):
         return "Depart Réparation"
+
 
 class start:
     def execute():
@@ -88,39 +95,42 @@ class start:
         numberBusesQueueR = 0
         statusControlCentre = False
         positionsRepairCentre = 0
-        billBook.append((arrivalBus, dateSimulation + expovariate(1/2)))
-		
+        billBook.append((arrivalBus, dateSimulation + expovariate(1 / 2)))
+
     def to_string():
         return "Debut"
+
 
 class end:
     def execute():
         global averageWaitingTimeControl, averageWaitingTimeRepair, rateUseRepair, averageSizeControlQueue, averageSizeRepairQueue
         billBook.clear()
-        try: 
+        try:
             averageWaitingTimeControl = areaNumberBusesQueueC / numberBuses
         except ZeroDivisionError:
             averageWaitingTimeControl = 0
         averageSizeControlQueue = areaNumberBusesQueueC / dateSimulation
-        try : 
+        try:
             averageWaitingTimeRepair = areaNumberBusesQueueR / numberBusesRepaired
         except ZeroDivisionError:
             averageWaitingTimeRepair = 0
         averageSizeRepairQueue = areaNumberBusesQueueR / dateSimulation
         rateUseRepair = areaPositionsRepairCentre / (2 * dateSimulation)
-	
+
     def __str__(self):
         return "Fin"
+
 
 def updating_areas(date1, date2):
     global areaNumberBusesQueueC, areaNumberBusesQueueR, areaPositionsRepairCentre
     areaNumberBusesQueueC += (date2 - date1) * numberBusesQueueC
     areaNumberBusesQueueR += (date2 - date1) * numberBusesQueueR
     areaPositionsRepairCentre += (date2 - date1) * positionsRepairCentre
-	
+
+
 def export_result_to_txt():
     global i, fileName, timeSimulation, numberBuses, numberBusesRepaired, numberBusesQueueC, numberBusesQueueR, averageWaitingTimeControl, averageWaitingTimeRepair, rateUseRepair, billBookExport
-    with open(fileName,"a+") as fic :
+    with open(fileName, "a+") as fic:
         fic.write('Simulation n° : {}\n'.format(i))
         fic.write('Durée de la simulation : {}\n'.format(timeSimulation))
         fic.write('Nombre de bus entrées : {}\n'.format(numberBuses))
@@ -131,13 +141,14 @@ def export_result_to_txt():
         fic.write('Taille moyenne file réparation : {}\n'.format(averageSizeRepairQueue))
         fic.write('Taux d\'utilisation du centre de réparation : {}\n'.format(rateUseRepair))
         fic.write('Echéancier de la simulation : {}\n\n'.format(billBookExport))
-		
+
+
 def export_final_result_to_txt():
     global fileName, dateSimulation, repeatSimulation, listNumberBuses, listNumberBusesRepaired, listNumberBusesQueueC, listNumberBusesQueueR, listAverageWaitingTimeControl, listAverageWaitingTimeRepair, listRateUserRepair
-    with open(fileName,"a+") as fic :
+    with open(fileName, "a+") as fic:
         fic.write('Nombre de répétition de la simulation : {}\n'.format(repeatSimulation))
         fic.write('Temps de simulation : {}\n'.format(dateSimulation))
-        
+
         # fic.write('Nombre de bus entrées pour chaque simulation : {}\n'.format(listNumberBuses))
         # fic.write('Nombre de bus réparés pour chaque simulation : {}\n'.format(listNumberBusesRepaired))
         # fic.write('Temps moyen attente file controle pour chaque simulation : {}\n'.format(listAverageWaitingTimeControl))
@@ -145,14 +156,21 @@ def export_final_result_to_txt():
         # fic.write('Temps moyen attente file réparation pour chaque simulation : {}\n'.format(listAverageWaitingTimeRepair))
         # fic.write('Taille moyenne file réparation pour chaque simulation : {}\n'.format(listAverageSizeRepairQueue))
         # fic.write('Taux d\'utilisation du centre de réparation pour chaque simulation : {}\n\n'.format(listRateUserRepair))
-        
-        fic.write('Moyenne du nombre de bus entrées : {} \n'.format(sum(listNumberBuses)/len(listNumberBuses)))
-        fic.write('Moyenne du nombre de bus réparés : {} \n'.format(sum(listNumberBusesRepaired)/len(listNumberBusesRepaired)))
-        fic.write('Moyenne du temps moyen attente file controle : {} \n'.format(sum(listAverageWaitingTimeControl)/len(listAverageWaitingTimeControl)))
-        fic.write('Moyenne de la taille moyenne file controle : {}\n'.format(sum(listAverageSizeControlQueue)/len(listAverageSizeControlQueue)))
-        fic.write('moyenne du temps moyen attente file réparation : {} \n'.format(sum(listAverageWaitingTimeRepair)/len(listAverageWaitingTimeRepair)))
-        fic.write('Moyenne de la taille moyenne file réparation : {}\n'.format(sum(listAverageSizeRepairQueue)/len(listAverageSizeRepairQueue)))
-        fic.write('Moyenne du taux d\'utilisation du centre de réparation : {}\n\n'.format(sum(listRateUserRepair)/len(listRateUserRepair)))
+
+        fic.write('Moyenne du nombre de bus entrées : {} \n'.format(sum(listNumberBuses) / len(listNumberBuses)))
+        fic.write('Moyenne du nombre de bus réparés : {} \n'.format(
+            sum(listNumberBusesRepaired) / len(listNumberBusesRepaired)))
+        fic.write('Moyenne du temps moyen attente file controle : {} \n'.format(
+            sum(listAverageWaitingTimeControl) / len(listAverageWaitingTimeControl)))
+        fic.write('Moyenne de la taille moyenne file controle : {}\n'.format(
+            sum(listAverageSizeControlQueue) / len(listAverageSizeControlQueue)))
+        fic.write('moyenne du temps moyen attente file réparation : {} \n'.format(
+            sum(listAverageWaitingTimeRepair) / len(listAverageWaitingTimeRepair)))
+        fic.write('Moyenne de la taille moyenne file réparation : {}\n'.format(
+            sum(listAverageSizeRepairQueue) / len(listAverageSizeRepairQueue)))
+        fic.write('Moyenne du taux d\'utilisation du centre de réparation : {}\n\n'.format(
+            sum(listRateUserRepair) / len(listRateUserRepair)))
+
 
 def add_result_to_lists():
     global numberBuses, numberBusesRepaired, numberBusesQueueC, numberBusesQueueR, averageWaitingTimeControl, averageWaitingTimeRepair, rateUseRepair, listNumberBuses, listNumberBusesRepaired, listNumberBusesQueueC, listNumberBusesQueueR, listAverageWaitingTimeControl, listAverageWaitingTimeRepair, listRateUserRepair
@@ -166,11 +184,21 @@ def add_result_to_lists():
     listAverageSizeRepairQueue.append(averageSizeRepairQueue)
     listRateUserRepair.append(rateUseRepair)
 
-#Paramètres de la simulation
-repeatSimulation = 500
-stopBusNumer= 500
 
-#initialisation des variables de la simulation
+# Paramètres de la simulation
+repeatSimulation = 500
+print(
+    "Veuillez indiquer le nombre de bus entrant dans le poste de contrôle à partir duquel vous souhaitez que la simulation s'arrête.")
+try:
+    stopBusNumber = int(input())
+    if stopBusNumber == 0:
+        print("Le nombre de bus doit être non nul.")
+        exit()
+except ValueError:
+    print("La valeur indiquée n'est pas un nombre.")
+    exit()
+
+# initialisation des variables de la simulation
 dateSimulation = 0
 numberBuses = 0
 numberBusesRepaired = 0
@@ -198,16 +226,16 @@ listAverageWaitingTimeRepair = []
 listAverageSizeRepairQueue = []
 listRateUserRepair = []
 
-#Variables pour l'export
-fileName = "SimulatorRepairCenter-{},{}.txt".format(stopBusNumer,repeatSimulation)
+# Variables pour l'export
+fileName = "SimulatorRepairCenter-{},{}.txt".format(stopBusNumber, repeatSimulation)
 billBookExport = []
 
 for i in range(0, repeatSimulation):
-    dateSimulation=0
+    dateSimulation = 0
     billBook.append((start, dateSimulation))
     while billBook:
-        billBook.sort(key = lambda date: date[1]) 
-        eventDate = billBook[0]	
+        billBook.sort(key=lambda date: date[1])
+        eventDate = billBook[0]
         billBookExport.append(billBook[0])
         del billBook[0]
         updating_areas(dateSimulation, eventDate[1])
